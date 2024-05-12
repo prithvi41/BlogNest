ab6018ab0dd7ca441a4119867081f856cb28f74c
Prithvi41 <rajp7163@gmail.com>
2024-05-12 10:32:55 +0530

    fixes

--- config/dbconnection.js
+++ config/dbconnection.js
@@ -7,6 +7,7 @@ const client = new Client ({
     host : process.env.PGHOST,
     port : process.env.PGPORT,
     user : process.env.PGUSER,
+    password : process.env.PGPASSWORD, 
     database : process.env.PGDB
 });
 
